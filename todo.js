d568a70354e32a2a5f9276ac8f6ce0c6028891aa
MaSsoumeh Zarei <47733451+MaSsoumeh@users.noreply.github.com>
2021-09-06 05:48:52 +0430

    make it fully functional using local storage

--- todo.js
+++ todo.js
@@ -1,73 +1,116 @@
-﻿ 
-    input = document.getElementById("myInput");
-    //  document.getElementById("addBtn").onclick = addNewItem;
-     input.addEventListener("keypress", addItemKeyPress);
-     
-     // Add new items
-     function addNewItem() {
- 
-       //  5 below lines are all the same:
-       getElement = input.value;
-       // const getElement = document.querySelector("input").value;
-       //  const newElement = document.getElementsByTagName("input")[0].value;
-       //  const newElement = document.getElementById("myInput").value;
-       //  const newElement = document.querySelectorAll("input")[0].value;
-       
-       if (getElement == "") {
-        
-          document.querySelector("input").placeholder = "Please Enter Something To Do ...";
-       } else {
-          let table = document.getElementById("mainTable");
-          let row1 = document.createElement("tr");
-          let col1 = document.createElement("td");
-          let col2 = document.createElement("td");
-          let delbtn = document.createElement("button");      
-          let newCheckBox = document.createElement("input");
-          newCheckBox.type = "checkbox";
-          newCheckBox.name = "tasks";
-          let txtnode = document.createTextNode(" \t " + getElement);
- 
-          // Style elements      
-          delbtn.className = "delbutton";
-          col1.className = "td1";
-          col2.className = "td2";
-          newCheckBox.className = "chkbox";
- 
-          // Append elements
-          table.appendChild(row1);
-          row1.appendChild(col1);
-          row1.appendChild(col2);
-          delbtn.appendChild(document.createTextNode("X"));
-          col1.appendChild(newCheckBox);
-          col1.appendChild(txtnode);
-          col2.appendChild(delbtn);
-         
-          // Restore to default settings
-          document.querySelector("input").value = "";
-          document.querySelector("input").placeholder = "New Task...";
-          
-           //Delete Item
-          delbtn.addEventListener("click", deleteItem);
-          function deleteItem() {
-             row1.classList.add("delete");
-          }
-          
-          // Cross out Item
-          col1.addEventListener("click", crossOut);
-          function crossOut() {
-             row1.classList.toggle("crossout");           
-          }
-       }
-    } //End of addNewItem()
- 
-    function addItemKeyPress(event) {
-       if (event.which ===13) {
-          //the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
-                addNewItem();
-       } 
-    } 
- 
- 
-         
-     
-  
+﻿
+document.getElementById("taskInput").addEventListener("keypress", onKeyPress);
+window.addEventListener("load", loadTasks);
+
+function addNewItem() {
+   const taskInput = document.getElementById("taskInput");
+   const newTask = taskInput.value.trim();
+
+   if (!newTask) {
+      document.querySelector("input").placeholder = "Please enter something to add to the list"
+      return;
+   }
+   const taskList = document.getElementById("taskList");
+   const listItem = createListItem(newTask, false);
+
+   taskList.appendChild(listItem);
+   saveTask(newTask);
+   resetForm();
+
+}
+
+function createListItem(task, isCompleted) {
+   const item = document.createElement("div");
+   item.className = "listItem";
+
+   const itemDescription = document.createElement("div");
+   itemDescription.className = "taskDescription";
+
+   const itemCheckBox = document.createElement("input");
+   itemCheckBox.type = "checkbox";
+   itemCheckBox.name = "task";
+   itemCheckBox.checked = isCompleted;
+   if (isCompleted) {
+      itemDescription.classList.add("crossout");
+   }
+   itemCheckBox.addEventListener("click", () => {
+      itemDescription.classList.toggle("crossout");
+      updateTaskStatus(task, itemCheckBox.checked);
+   });
+
+   const descriptionText = document.createTextNode(` \t ${task}`);
+   const descriptionElement = document.createElement("div");
+   descriptionElement.appendChild(descriptionText)
+   descriptionElement.className = "descriptionText"
+
+   const img = document.createElement('img');
+   img.src = './images/trash.png';
+   img.alt = 'Button Image';
+   img.style.width = '24px';
+   img.style.height = '24px';
+
+   const itemDeleteBtn = document.createElement("button");
+   itemDeleteBtn.className = "deleteButton";
+   itemDeleteBtn.addEventListener("click", () => {
+      item.remove();
+      deleteTask(task);
+   });
+   itemDeleteBtn.appendChild(img);
+
+
+   itemDescription.appendChild(itemCheckBox);
+   itemDescription.appendChild(descriptionElement);
+   item.appendChild(itemDescription);
+   item.appendChild(itemDeleteBtn);
+
+   return item;
+}
+
+
+function onKeyPress(event) {
+   // 13 is the enter key's keycode, this could also be display by event.keyCode === 13
+   if (event.which === 13) {
+      addNewItem();
+   }
+}
+
+function resetForm() {
+   const taskInput = document.getElementById("taskInput");
+   taskInput.value = "";
+   taskInput.placeholder = "New Task...";
+}
+
+function saveTask(task, isCompleted) {
+   let tasks = getTasksFromStorage()
+   tasks.push({ task, isCompleted });
+   saveTasksToStorage(tasks)
+}
+
+function loadTasks() {
+   let tasks = getTasksFromStorage()
+   const taskList = document.getElementById("taskList");
+   tasks.forEach(({ task, isCompleted }) => {
+      const listItem = createListItem(task, isCompleted);
+      taskList.appendChild(listItem);
+   });
+}
+
+function deleteTask(task) {
+   let tasks = getTasksFromStorage()
+   tasks = tasks.filter(t => t.task !== task);
+   saveTasksToStorage(tasks)
+}
+
+function updateTaskStatus(task, isCompleted) {
+   let tasks = getTasksFromStorage()
+   tasks = tasks.map(t => t.task === task ? { task, isCompleted } : t);
+   saveTasksToStorage(tasks)
+}
+
+function getTasksFromStorage() {
+   return JSON.parse(localStorage.getItem("tasks")) || [];
+}
+
+function saveTasksToStorage(tasks) {
+   localStorage.setItem("tasks", JSON.stringify(tasks));
+}
